97b6277bfebff6a67eea9205d175cfa205f454d5
YiwenDong <dong.yiw@husky.neu.edu>
2018-08-05 20:59:20 -0400

    Created repo for different models

---
+++ nodesrc/repository/strain.repository.js
@@ -0,0 +1,45 @@
+const strainModel = require('../model/strain.model');
+
+findAllStrains = () => {
+  return strainModel.find();//.then((err, strains) => {
+    
+//  });
+}
+
+findStrain = (name) => {
+  return strainModel.findById(name);
+}
+
+saveStrain = (strain) => {
+  return strainModel(strain).save();
+}
+
+upsertStrain = (strain) => {
+  return strainModel.findByIdAndUpdate(strain._id, strain, {upsert:true}).exec();
+}
+
+saveStrains = (strains) => {
+  strains.forEach((strain) => {
+    saveStrain(strain).then((err) => {if (err) {throw JSON.stringify(err);}});
+  });
+}
+
+upsertStrains = (strains) => {
+  strains.forEach((strain) => {
+    upsertStrain(strain).then((err) => {});
+  });
+}
+
+searchStrainsByName = (q) => {
+  return strains.find({_id: new RegExp('^'+q+'$', "i")})
+}
+
+module.exports = {
+  findAllStrains,
+  findStrain, 
+  saveStrain,
+  saveStrains,
+  upsertStrain,
+  upsertStrains,
+  searchStrainsByName
+}
