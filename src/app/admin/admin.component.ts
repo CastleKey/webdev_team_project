1ad513df3ce28fb61d8756db17df278a7d6f6e1a
CastleKey <izaak.lofgren@gmail.com>
2018-08-10 15:08:15 -0400

    Added framework components of admin console

---
+++ src/app/admin/admin.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import {Router} from "@angular/router"
+import {UserService} from "../services/user.service";
+
+@Component({
+  selector: 'app-admin',
+  templateUrl: './admin.component.html',
+  styleUrls: ['./admin.component.css']
+})
+export class AdminComponent implements OnInit {
+
+  username: String;
+  password: String;
+  err;
+
+  constructor(private router: Router,
+              private userService: UserService) { }
+
+  ngOnInit() {
+  }
+
+  // login(username, password) {
+  //   const user = {
+  //     username: username,
+  //     password: password
+  //   };
+  //   this.userService.login(user)
+  //       .then(u => {
+  //         this.userService.update(u);
+  //         this.router.navigate(['profile'])
+  //       })
+  //       .catch(e => this.err = true);
+  // }
+
+}
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,22 @@
 import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
- 
+
 import { HomeComponent }   from './home/home.component';
 import { StrainComponent }   from './strain/strain.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { ProfileComponent } from './profile/profile.component';
- 
+import { AdminComponent } from './admin/admin.component';
+
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'strain/:strain_name', component: StrainComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'profile', component: ProfileComponent }
+  { path: 'profile', component: ProfileComponent },
+  { path: 'admin', component: AdminComponent }
 ];
- 
+
 @NgModule({
   imports: [ RouterModule.forRoot(routes) ],
   exports: [ RouterModule ]
